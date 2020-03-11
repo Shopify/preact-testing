c3b14370a61d4b27b24c6c7dd0daca6d0a3a661d
Mathew  Allen <mathew.allen@shopify.com>
2020-03-11 14:56:59 -0400

    🔥 react types that are never useful anyway

--- src/matchers/index.ts
+++ src/matchers/index.ts
@@ -1,9 +1,5 @@
 import 'jest';
 import {ComponentType, Context as PreactContext} from 'preact';
-import {
-  ComponentType as ReactComponentType,
-  Context as ReactContext,
-} from 'react';
 import {Node, PropsFor} from '../types';
 
 import {toHaveProps, toHaveDataProps} from './props';
@@ -23,38 +19,28 @@ declare global {
         type: Type,
         props?: Partial<PropsFor<Type>>,
       ): void;
-      toContainComponent<Type extends string | ReactComponentType<any>>(
-        type: Type,
-        props?: Partial<PropsFor<Type>>,
-      ): void;
       toContainComponentTimes<Type extends string | ComponentType<any>>(
         type: Type,
         times: number,
         props?: Partial<PropsFor<Type>>,
       ): void;
-      toContainComponentTimes<Type extends string | ReactComponentType<any>>(
-        type: Type,
-        times: number,
-        props?: Partial<PropsFor<Type>>,
-      ): void;
       toProvideContext<Type>(context: PreactContext<Type>, value?: Type): void;
-      toProvideContext<Type>(context: ReactContext<Type>, value?: Type): void;
       toContainText(text: string): void;
       toContainHtml(text: string): void;
 
-      // compatibility APIs for react-testing
+      // compatibility APIs for @shopify/react-testing
       toHaveReactProps(props: Partial<PropsFromNode<R>>): void;
       toHaveReactDataProps(data: {[key: string]: string}): void;
-      toContainReactComponent<Type extends string | ReactComponentType<any>>(
+      toContainReactComponent<Type extends string | ComponentType<any>>(
         type: Type,
         props?: Partial<PropsFor<Type>>,
       ): void;
-      toContainReactComponentTimes<Type extends string | ReactComponentType<any>>(
+      toContainReactComponentTimes<Type extends string | ComponentType<any>>(
         type: Type,
         times: number,
         props?: Partial<PropsFor<Type>>,
       ): void;
-      toProvideReactContext<Type>(context: ReactContext<Type>, value?: Type): void;
+      toProvideReactContext<Type>(context: PreactContext<Type>, value?: Type): void;
       toContainReactText(text: string): void;
       toContainReactHtml(text: string): void;
     }
